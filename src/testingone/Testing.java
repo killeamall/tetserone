a783b4d8ef17be93897a9a90f95dcd61367f2779
killeamall <gopinathanraman13790>
2018-12-30 11:32:14 +0530

    test

---
+++ src/testingone/Testing.java
@@ -0,0 +1,16 @@
+package testingone;
+
+public class Testing {
+public static void main(String[] args) {
+	
+
+		// TODO Auto-generated method stub
+
+	
+	int a=21;
+	String l= "habs";
+	System.out.println(l);
+
+}
+	
+}
